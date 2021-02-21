a754e9409a864ec3dc4eef8235203286f9182a7a
chang <yingjie.chang@qunar.com>
2021-02-21 11:50:08 +0800

    链表反转

---
+++ src/code/Solution.java
@@ -0,0 +1,12 @@
+package code;
+
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        System.out.println("a");
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
